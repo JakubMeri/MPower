fed3b151990fca315b1e2fc0976eb81f73a8de78
JM Coding <jakubmeri@gmail.com>
2020-05-20 22:18:31 +0200

    Updates

---
+++ precache-manifest.3766895bdbf1938c7db23d7c712a1088.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d1d3fe70a99dc4b63c896cfddb4b06e",
+    "url": "/MPower/index.html"
+  },
+  {
+    "revision": "85a35d1158a529c9b743",
+    "url": "/MPower/static/css/main.da4524ea.chunk.css"
+  },
+  {
+    "revision": "29a9a20ab1bb789f41ae",
+    "url": "/MPower/static/js/2.389cab8a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/MPower/static/js/2.389cab8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85a35d1158a529c9b743",
+    "url": "/MPower/static/js/main.3f45e5c3.chunk.js"
+  },
+  {
+    "revision": "d8ea9daf499f0cd244fb",
+    "url": "/MPower/static/js/runtime-main.c89fe9bb.js"
+  },
+  {
+    "revision": "11021a5d736552617b2987261aeffad9",
+    "url": "/MPower/static/media/open.11021a5d.jpeg"
+  }
+]);
